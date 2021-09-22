c60bfd800cc4852f281b6098118a5754c1b3df16
Eric Timmons <etimmons@mit.edu>
2021-09-22 10:36:08 -0400

    POSIX: Add MAKEDEV, MAJOR, and MINOR

--- posix/windows.lisp
+++ posix/windows.lisp
@@ -31,9 +31,9 @@
   fchmod fchownat fcntl posix-fallocate fork fstatat fstatvfs fsync futimens getdomainname
   getegid geteuid getgid getgrgid getgrnam getpagesize getpgid getpgrp getpid getppid
   getpriority getpwnam getpwuid getrlimit getrusage gettimeofday getuid ioctl link linkat
-  lockf lstat mkdirat mkdtemp mkfifo mkfifoat mknod mknodat mkstemp mlock mlockall mmap
-  mprotect msync munlock munlockall munmap nice openat opendir openlog pread pwrite readdir
-  readlink readlinkat readv renameat rewinddir seekdir select setegid setenv seteuid
-  setgid setlogmask setpgid setpgrp setpriority setregid setreuid setrlimit setsid setuid
-  statvfs symlink symlinkat sync sysconf syslog telldir truncate uname unlinkat unsetenv
-  usleep utimensat utimes writev s-issock s-islnk gethostname)
+  lockf lstat makedev major minor mkdirat mkdtemp mkfifo mkfifoat mknod mknodat mkstemp
+  mlock mlockall mmap mprotect msync munlock munlockall munmap nice openat opendir openlog
+  pread pwrite readdir readlink readlinkat readv renameat rewinddir seekdir select setegid
+  setenv seteuid setgid setlogmask setpgid setpgrp setpriority setregid setreuid setrlimit
+  setsid setuid statvfs symlink symlinkat sync sysconf syslog telldir truncate uname
+  unlinkat unsetenv usleep utimensat utimes writev s-issock s-islnk gethostname)
